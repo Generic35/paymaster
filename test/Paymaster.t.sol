34737631de90a2682087774888a6f71973805f9d
Generic35 <ricklamontagne@gmail.com>
2025-09-30 17:15:58 -0400

    feat: revert user op gas to what it was

--- test/Paymaster.t.sol
+++ test/Paymaster.t.sol
@@ -80,7 +80,7 @@ contract PaymasterTest is Test {
         UserOperation memory userOp = createUserOp();
         signUserOp(userOp);
 
-        vm.expectRevert(createEncodedValidationResult(false, 52981));
+        vm.expectRevert(createEncodedValidationResult(false, 53025));
         entrypoint.simulateValidation(userOp);
     }
 
@@ -90,7 +90,7 @@ contract PaymasterTest is Test {
         userOp.paymasterAndData = abi.encodePacked(address(paymaster), abi.encode(MOCK_VALID_UNTIL, MOCK_VALID_AFTER), r, s, v);
         signUserOp(userOp);
 
-        vm.expectRevert(createEncodedValidationResult(true, 52991));
+        vm.expectRevert(createEncodedValidationResult(true, 53035));
         entrypoint.simulateValidation(userOp);
     }
 
